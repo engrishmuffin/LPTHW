6dc3eac7f1c079005c94a0eaf4a263c50e840971
caedmon <caedwebb@gmail.com>
2016-11-03 16:22:35 -0700

    Ex17 now 6 lines. Much more legible than 1line ver

--- ex17.py
+++ ex17.py
@@ -1,1 +1,11 @@
-from sys import argv ; from os.path import exists ; script, from_file, to_file = argv ; print "Copying from %s to %s" % (from_file, to_file) ; indata = open(from_file).read() ; print "the input file is %d bytes long" % len(indata), "\n", "does the output file exist? %r" % exists(to_file), "\n", "Ready, hit RETURN to continue, CTRL-C to abort." ; raw_input() ; out_file = open(to_file, 'w').write(indata) ; print "Alright, all done."
+#import argv and exists features.
+from sys import argv ; from os.path import exists
+script, from_file, to_file = argv
+print "Copying from %s to %s" % (from_file, to_file)
+# define indata as contents of opened from_file
+indata = open(from_file).read()
+# read and print the length of indata
+# check if to_file exists, prompt user to quit/continue.
+print "the input file is %d bytes long" % len(indata), "\n", "does the output file exist? %r" % exists(to_file), "\n", "Ready, hit RETURN to continue, CTRL-C to abort." ; raw_input()
+# define out_file by opening to_file, make writeable, write.
+out_file = open(to_file, 'w').write(indata) ; print "Done."
