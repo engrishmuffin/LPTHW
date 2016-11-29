157b9c2b559592a7fc45a24f082fc2f7fa71164e
caedmon <caedwebb@gmail.com>
2016-11-29 15:21:33 -0800

    Cleaned up ex5 whitespace.

--- ex5.py
+++ ex5.py
@@ -8,11 +8,11 @@ hair = 'Black'
 meters = height * 2.54
 kilos = weight / 2.24
 
-print "Let's talk about %s." %name
-print "He's %d inches tall." %height
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
 print "That's %d centimeters." % meters
-print "He's %d pounds heavy." %weight
-print "Or %d kilograms." %kilos
+print "He's %d pounds heavy." % weight
+print "Or %d kilograms." % kilos
 print "Actually, that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usuall %s depending on the coffee." % teeth
