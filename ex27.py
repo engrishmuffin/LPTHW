0b007bdf2f4ea40951deb26be153f74e661693b2
caedmon <caedwebb@gmail.com>
2016-12-14 14:07:59 -0800

    Completed Ex28. 19/20 correct! (misread #4).

---
+++ ex27.py
@@ -0,0 +1,50 @@
+True and True
+print "True"
+
+False and True
+print "False"
+
+1 == 1 and 2 ==1
+print "False"
+
+1 == 1 or 2!= 1
+print "False -  nope! True"
+
+True and 1 == 1
+print "True"
+
+False and 0 != 0
+print "False"
+
+True or 1 == 1
+print "True"
+
+"test" == "testing"
+print "False"
+
+1 != 0 and 2 == 1
+print "False"
+
+"test" == 1
+print "True"
+
+not (True and False)
+print "True"
+
+not (1 == 1 and 0 != 1)
+print "False"
+
+not (10 == 1 or 1000 == 1000)
+print "False"
+
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+print "True"
+
+1 == 1 and (not ("testing" == 1 or 1 == 0))
+print "True"
+
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+print "False"
+
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+print "False"
